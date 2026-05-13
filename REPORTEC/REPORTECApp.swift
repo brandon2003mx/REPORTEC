3346c6f973cc44ccd3c5b6de7d86cb0f97ddd7e3
Brandon Flores <brandon2003mx@gmail.com>
2026-05-13 05:16:54 +0000

    Initial Commit

---
+++ REPORTEC/REPORTECApp.swift
@@ -0,0 +1,17 @@
+//
+//  REPORTECApp.swift
+//  REPORTEC
+//
+//  Created by ec2-user on 5/13/26.
+//
+
+import SwiftUI
+
+@main
+struct REPORTECApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
